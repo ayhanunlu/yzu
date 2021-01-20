dff5e71f4a9c135a452ea6a387376340dea237ed
ayhanunlu <ayhan@trakya.edu.tr>
2021-01-20 14:27:08 +0300

    üçünücü hali

--- rehberson.py
+++ rehberson.py
@@ -7,10 +7,19 @@ def veriekle():
     frame1=Frame(pencere)
     ad=Label(frame1,text="Ad-Soyad:",font="Times 20")
     adtext=Entry(frame1,font="Times 20")
+    tel=Label(frame1,text="Tel:",font="Times 20")
+    teltext=Entry(frame1,font="Times 20")
+    email=Label(frame1,text="E-mail:",font="Times 20")
+    emailtext=Entry(frame1,font="Times 20")
+
 
     ad.grid(row=1,column=1,padx=10,pady=10)
     adtext.grid(row=1,column=2,padx=0)
-    
+    tel.grid(row=2,column=1,padx=10,pady=10)
+    teltext.grid(row=2,column=2,padx=0)
+    email.grid(row=3,column=1,padx=10,pady=10)
+    emailtext.grid(row=3,column=2,padx=0)
+
     frame1.grid()
     
     frame1.mainloop()
